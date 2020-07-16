6c78c20ecb23a97d7b373bab803143c1bcd9ad1c
johnny5581 <johnny5581@gmai.com>
2020-07-16 17:00:49 +0800

    新增直接註冊Property為HardWord的功能

--- src/Ancestor.Core/HardWordManager.cs
+++ src/Ancestor.Core/HardWordManager.cs
@@ -2,7 +2,9 @@
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Text;
 
 namespace Ancestor.Core
@@ -14,15 +16,27 @@ namespace Ancestor.Core
             = new ConcurrentDictionary<PropertyInfo, HardWordAttribute>();
         private static readonly ConcurrentDictionary<Type, PropertyInfo[]> PropertyCaches
             = new ConcurrentDictionary<Type, PropertyInfo[]>();
-
+        private static readonly Dictionary<PropertyInfo, HardWordAttribute> RegisteredProperties
+            = new Dictionary<PropertyInfo, HardWordAttribute>();
         public static Func<PropertyInfo, HardWordAttribute> HardWordResolver
         {
             get { return _hardwordResolver ?? GetHardWordAttribute; }
             set { _hardwordResolver = value; }
         }
+
         public static HardWordAttribute Get(PropertyInfo property)
         {
             return HardWordResolver(property);
+        }
+        public static void RegisterProperty(PropertyInfo property, HardWordAttribute attr = null)
+        {
+            if (!RegisteredProperties.ContainsKey(property))
+                RegisteredProperties.Add(property, attr ?? new HardWordAttribute());
+        }
+        public static void RegisterProperty<T>(Expression<Func<T, object>> propertySelector, HardWordAttribute attr = null)
+        {
+            var property = GetPropertyInfoFromExpression(propertySelector);
+            RegisterProperty(property, attr);
         }
         public static IDictionary<PropertyInfo, HardWordAttribute> Get(Type type)
         {
@@ -46,9 +60,31 @@ namespace Ancestor.Core
             if (!AttributeCaches.TryGetValue(property, out attr))
             {
                 attr = property.GetCustomAttributes(typeof(HardWordAttribute), false).FirstOrDefault() as HardWordAttribute;
+                if (attr == null)
+                    RegisteredProperties.TryGetValue(property, out attr);
                 AttributeCaches.AddOrUpdate(property, attr, (k, v) => attr);
             }
             return attr;
+        }
+        private static PropertyInfo GetPropertyInfoFromExpression(LambdaExpression lambda)
+        {
+            var memberExp = ExtractMemberExpression(lambda.Body);
+            if (memberExp == null)
+                throw new ArgumentException("must be member access expression");
+            if (memberExp.Member.DeclaringType == null)
+                throw new InvalidOperationException("property does not have declaring type");
+            return memberExp.Member.DeclaringType.GetProperty(memberExp.Member.Name);
+        }
+        private static MemberExpression ExtractMemberExpression(Expression expression)
+        {
+            if (expression.NodeType == ExpressionType.MemberAccess)
+                return ((MemberExpression)expression);
+            if (expression.NodeType == ExpressionType.Convert)
+            {
+                var operand = ((UnaryExpression)expression).Operand;
+                return ExtractMemberExpression(operand);
+            }
+            return null;
         }
     }
 }
