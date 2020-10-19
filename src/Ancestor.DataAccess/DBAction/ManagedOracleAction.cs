9953081a1fb3017a4d707609b0f3fd4ab51d51d2
johnny5581 <johnny5581@gmai.com>
2020-10-19 17:47:07 +0800

    修正OracleHome路徑取得

--- src/Ancestor.DataAccess/DBAction/ManagedOracleAction.cs
+++ src/Ancestor.DataAccess/DBAction/ManagedOracleAction.cs
@@ -146,7 +146,7 @@ namespace Ancestor.DataAccess.DBAction
                         var netadmin = Path.Combine(p, "..", "NETWORK", "ADMIN");
                         if (Directory.Exists(netadmin))
                         {
-                            oracleHome = p;
+                            oracleHome = Path.GetDirectoryName(p);
                             break;
                         }
                     }
