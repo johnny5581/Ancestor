66ab124371f7d8b12b7a4150e1540241ec9a9527
Nagi Lin <johnny5581@gmail.com>
2024-02-24 11:09:50 +0800

    use Exception.Data for AncestorException.Code

--- src/Ancestor.Core/AncestorException.cs
+++ src/Ancestor.Core/AncestorException.cs
@@ -13,7 +13,27 @@ namespace Ancestor.Core
     [Serializable]
     public class AncestorException : Exception
     {
-        public int Code { get; private set; }        
+        public static readonly int CodeUniqueConstraintDuplicated = 13001;
+
+        private static readonly int[] KnownCodes
+            = typeof(AncestorException).GetFields(System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.DeclaredOnly | System.Reflection.BindingFlags.Static)
+            .Select(field => field.GetValue(null)).Cast<int>().ToArray();
+
+        public int Code
+        {
+            get
+            {
+                return Data.Contains("code")
+                    ? (int)Data["code"] : 0;
+            }
+            set
+            {
+                if (Data.Contains("code"))
+                    Data["code"] = value;
+                else
+                    Data.Add("code", value);
+            }
+        }
 
         public AncestorException(int code, string message) : base(message)
         {
@@ -26,12 +46,14 @@ namespace Ancestor.Core
 
         protected AncestorException(SerializationInfo info, StreamingContext context)
         {
-            info.AddValue("code", Code);
+
         }
-        public override void GetObjectData(SerializationInfo info, StreamingContext context)
+
+        public bool IsUnknwonCode
         {
-            Code = info.GetInt32("code");
-            base.GetObjectData(info, context);
+            get { return !KnownCodes.Contains(Code); }
         }
+
+
     }
 }
